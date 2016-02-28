bb68eee94f58c4d3743d8b7f9926af5ba3e6af19
Andrew Silluron <asilluron@users.noreply.github.com>
2016-02-27 16:32:31 -0800

    Setup coverage and junit reporter

--- karma.conf.js
+++ karma.conf.js
@@ -26,7 +26,7 @@ var karmaConfig = {
   },
   reporters: ['dots', 'coverage'],
   coverageReporter: {
-    type: 'html',
+    type: 'lcov',
     dir: 'build/coverage/'
   },
   webpack: webpackConfig,
